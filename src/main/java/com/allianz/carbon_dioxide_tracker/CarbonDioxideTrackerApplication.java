1008fff43dafc0b7ef43e6921c9ce02df16b4653
Sahaya Anto Robinson <anto_robinson@outlook.com>
2021-01-20 21:50:06 +0530

    updating project names

--- src/main/java/com/allianz/carbon_dioxide_tracker/CarbonDioxideTrackerApplication.java
+++ src/main/java/com/allianz/carbon_dioxide_tracker/CarbonDioxideTrackerApplication.java
@@ -1,13 +1,13 @@
-package com.collaborativeChallenge.co2ConcentrationApp;
+package com.allianz.carbon_dioxide_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Co2ConcentrationAppApplication {
+public class CarbonDioxideTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Co2ConcentrationAppApplication.class, args);
+		SpringApplication.run(CarbonDioxideTrackerApplication.class, args);
 	}
 
 }
