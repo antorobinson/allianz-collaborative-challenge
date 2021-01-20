4c5c7832ddf5e5179da174fa373e3b3819f92eda
Sahaya Anto Robinson <anto_robinson@outlook.com>
2021-01-20 18:04:13 +0530

    Setting up the basic project

---
+++ src/main/java/com/collaborativeChallenge/co2ConcentrationApp/Co2ConcentrationAppApplication.java
@@ -0,0 +1,13 @@
+package com.collaborativeChallenge.co2ConcentrationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2ConcentrationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2ConcentrationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collaborativeChallenge/co2ConcentrationApp/Co2ConcentrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collaborativeChallenge.co2ConcentrationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2ConcentrationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
