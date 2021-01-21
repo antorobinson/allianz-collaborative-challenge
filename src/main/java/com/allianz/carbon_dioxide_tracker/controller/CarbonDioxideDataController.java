fccf657926940dd5c59410398dc91c1aa347435e
Sahaya Anto Robinson <anto_robinson@outlook.com>
2021-01-21 21:43:28 +0530

    updated api endpoints

--- src/main/java/com/allianz/carbon_dioxide_tracker/controller/CarbonDioxideDataController.java
+++ src/main/java/com/allianz/carbon_dioxide_tracker/controller/CarbonDioxideDataController.java
@@ -1,11 +1,26 @@
 package com.allianz.carbon_dioxide_tracker.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/")
+@RequestMapping("/v1/carbonDioxideData")
 public class CarbonDioxideDataController {
 
+	@PostMapping("/SensorReadings")
+	public ResponseEntity<String> addReading(){
+		
+		return ResponseEntity.status(201).body("Success");
+	}
+	
+	@GetMapping("/SensorReadings/reading")
+	public ResponseEntity<String> getReadingPerCity(){
+		
+		return ResponseEntity.ok().body("Success");
+	}
+	
 	
 }
