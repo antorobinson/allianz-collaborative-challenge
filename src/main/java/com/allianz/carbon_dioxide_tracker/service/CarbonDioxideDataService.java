54750629b226b26323e4c69dde9b0f51e1022fd6
Sahaya Anto Robinson <anto_robinson@outlook.com>
2021-01-21 21:59:19 +0530

    added method to a service class

--- src/main/java/com/allianz/carbon_dioxide_tracker/service/CarbonDioxideDataService.java
+++ src/main/java/com/allianz/carbon_dioxide_tracker/service/CarbonDioxideDataService.java
@@ -1,5 +1,12 @@
 package com.allianz.carbon_dioxide_tracker.service;
 
+import org.json.JSONObject;
+
 public class CarbonDioxideDataService {
+	
+	public JSONObject addDevice(){
+		return null;
+		
+	}
 
 }
